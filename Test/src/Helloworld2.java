9f8ff8d622f3c355ce5c96e50f6e9bdae475894f
Simone Festa <55951548+simonefesta@users.noreply.github.com>
2019-11-14 23:39:21 +0100

    Update Helloworld2.java

--- Test/src/Helloworld2.java
+++ Test/src/Helloworld2.java
@@ -1,8 +1,8 @@
-
+package main_pkg;
 public class Helloworld2 {
 
 		public static void main(String[] args) {
-			System.out.println("Hello World");
+			System.console().writer().println("Hello world!");
 
 		}
 
